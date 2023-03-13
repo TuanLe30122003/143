f9f09baf47018cca743b120768d84ae6fdc2df7c
TuanLe30122003 <91524625+TuanLe30122003@users.noreply.github.com>
2023-03-13 17:58:48 +0700

    Updates

---
+++ precache-manifest.d2740a7db029613da3bdbfac2211ca21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d3e9a8b616a1111e885",
+    "url": "/TuanLe30122003/143/static/css/main.a02cbeba.chunk.css"
+  },
+  {
+    "revision": "5d3e9a8b616a1111e885",
+    "url": "/TuanLe30122003/143/static/js/main.5d3e9a8b.chunk.js"
+  },
+  {
+    "revision": "975f63afe5057efb9a20",
+    "url": "/TuanLe30122003/143/static/js/1.975f63af.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TuanLe30122003/143/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "bfe5dda409da4160d860",
+    "url": "/TuanLe30122003/143/static/js/runtime~main.bfe5dda4.js"
+  },
+  {
+    "revision": "de3ec38a231d6e70555e80719530bfb0",
+    "url": "/TuanLe30122003/143/index.html"
+  }
+];
